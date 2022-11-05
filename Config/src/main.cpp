78964d9545dc3b22d7d27256510a6fbfe6f695c4
xTigerFace <34776435+xTigerFace@users.noreply.github.com>
2022-11-05 08:00:48 -0400

    Tested gui
    Save files untested

--- Config/src/main.cpp
+++ Config/src/main.cpp
@@ -1,43 +1,5 @@
 #include "config-graphics-core.h"
 
-vector<MultiButton> mainMenuButtons = {
-  {
-    0, 0, 120, 120, 5, 0,
-    {"Red", "Blue", "Skills", "Test", "None"},
-    {color(255, 255, 255), color(255, 255, 255), color(255, 255, 255), color(255, 255, 255), color(255, 255, 255)},
-    {color(255, 0, 0), color(0, 0, 255), color(27, 62, 89), color(7, 94, 7), color(28, 27, 3)},
-    mono20
-  },
-  {
-    120, 0, 120, 120, 2, 0, 
-    {"Left", "Right"},
-    {color(255, 255, 255), color(255, 255, 255)},
-    {color(66, 15, 23), color(10, 76, 84)},
-    mono20
-  },
-  {
-    240, 0, 120, 120, 2, 0, 
-    {"Roller", "High Goal"},
-    {color(255, 255, 255), color(255, 255, 255)},
-    {color(66, 15, 23), color(10, 76, 84)},
-    mono20
-  },
-  {
-    360, 0, 120, 120, 2, 0, 
-    {"Main", "Other"},
-    {color(255, 255, 255), color(255, 255, 255)},
-    {color(66, 15, 23), color(10, 76, 84)},
-    mono20
-  },
-  {
-    0, 120, 120, 120, 1, 0,
-    {"SAVE [X]"},
-    {color(255, 255, 255)},
-    {color(10, 61, 35)},
-    mono20
-  }
-};
-
 int main() {
   if (!Brain.SDcard.isInserted()) {
     Brain.Screen.setFont(mono60);
