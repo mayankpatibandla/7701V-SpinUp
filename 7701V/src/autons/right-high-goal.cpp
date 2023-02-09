cdbe61bdedff7ea8d3325039b970da8a1eb8884c
xTigerFace <34776435+xTigerFace@users.noreply.github.com>
2023-02-09 15:21:11 -0500

    Developed right high goal auton

--- 7701V/src/autons/right-high-goal.cpp
+++ 7701V/src/autons/right-high-goal.cpp
@@ -1,44 +1,52 @@
 #include "auton-manager.h"
 
 void autons::rightHighGoal() {
-  flyMtrs.spin(fwd, 0.875 * 12, volt);
-  angler.set(true);
+  timer autonTimer;
+  flyMtrs.spin(fwd, 0.935 * 12, volt);
 
   // roller
   driveRelative(23, 300, 1700);
-  turnToAngle(-M_PI_2 + 0.1, 600, 1500);
+  turnToAngle(-M_PI_2 + 0.1, 600, 1200);
 
   driveRelative(4, 150, 500);
   intakeMtrs.spin(fwd, -12, volt);
-  this_thread::sleep_for(275);
+  this_thread::sleep_for(300);
   intakeMtrs.stop(brake);
 
-  driveRelative(-3, 0, 500);
+  driveRelative(-2.4166, 0, 1000);
 
   // shoot preloads
-  turnToAngle(-1.78, 100, 700);
+  turnToAngle(-1.8, 200, 700);
+  this_thread::sleep_for(750);
   Indexer.shootDisc();
-  this_thread::sleep_for(2000);
+  this_thread::sleep_for(250);
+  angler.set(true);
+  this_thread::sleep_for(250);
   Indexer.shootDisc();
-  this_thread::sleep_for(500);
+  this_thread::sleep_for(250);
+  angler.set(false);
 
-  // intake more discs
+  // intake discs
   // 2.67, 2.46, -2.28
   intakeMtrs.spin(fwd, 12, volt);
-  flyMtrs.spin(fwd, 0.775 * 12, volt);
-
-  turnToAngle(2.67, 250, 1000);
-  driveRelative(25, 300, 1000, {.0005, 0.000001, 0.000005});
-
-  turnToAngle(2.46, 250, 1000);
-  driveRelative(50, 400, 1500, {.0005, 0.000001, 0.000005});
+  flyMtrs.spin(fwd, 0.85 * 12, volt);
+  turnToAngle(2.9, 250, 1000);
+  // driveRelative(80, 400, 2500, {0.0005, 0.000001, 0.000025});
+  driveMtrs.spin(fwd, 6, volt);
+  this_thread::sleep_for(2700);
+  driveMtrs.stop();
 
   //shoot discs
+  turnToAngle(-2.325, 300, 1500, {0.8, 0, 0.0075});//-2.325
   this_thread::sleep_for(100);
-  turnToAngle(-2.0, 300, 1000);
+  // 111.25, 34 + 13/16
+  // turnToAngle(M_PI + std::atan2((54+13/16) - pt::y(), 111.25 - pt::x()), 300, 1500);
   Indexer.shootDisc();
   this_thread::sleep_for(1000);
   Indexer.shootDisc();
   this_thread::sleep_for(1000);
   Indexer.shootDisc();
+  Controller.Screen.clearLine(3);
+  Controller.Screen.print(autonTimer.time(sec));
+  this_thread::sleep_for(1000); 
 }
