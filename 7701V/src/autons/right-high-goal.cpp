3cbaecdc2a7f3edbf06987dc0120da6d16ee40b2
xTigerFace <34776435+xTigerFace@users.noreply.github.com>
2023-02-12 15:12:48 -0500

    Added skills auton + improved right high goal auton

--- 7701V/src/autons/right-high-goal.cpp
+++ 7701V/src/autons/right-high-goal.cpp
@@ -2,13 +2,13 @@
 
 void autons::rightHighGoal() {
   timer autonTimer;
-  flyMtrs.spin(fwd, 0.935 * 12, volt);
+  flyMtrs.spin(fwd, 0.9125 * 12, volt);
 
   // roller
   driveRelative(23, 300, 1700);
   turnToAngle(-M_PI_2 + 0.1, 600, 1200);
 
-  driveRelative(4, 150, 500);
+  driveRelative(12, 150, 600);
   intakeMtrs.spin(fwd, -12, volt);
   this_thread::sleep_for(300);
   intakeMtrs.stop(brake);
@@ -30,14 +30,14 @@ void autons::rightHighGoal() {
   // 2.67, 2.46, -2.28
   intakeMtrs.spin(fwd, 12, volt);
   flyMtrs.spin(fwd, 0.85 * 12, volt);
-  turnToAngle(2.9, 250, 1000);
+  turnToAngle(2.815, 250, 1750);
   // driveRelative(80, 400, 2500, {0.0005, 0.000001, 0.000025});
   driveMtrs.spin(fwd, 6, volt);
-  this_thread::sleep_for(2700);
+  this_thread::sleep_for(2150);
   driveMtrs.stop();
 
   //shoot discs
-  turnToAngle(-2.325, 300, 1500, {0.8, 0, 0.0075});//-2.325
+  turnToAngle(-1.9125, 300, 1500, {0.8, 0, 0.0075});//-2.325
   this_thread::sleep_for(100);
   // 111.25, 34 + 13/16
   // turnToAngle(M_PI + std::atan2((54+13/16) - pt::y(), 111.25 - pt::x()), 300, 1500);
