6ae3c45038f50ea08abfb42cdd7d25c03ccd7148
xTigerFace <34776435+xTigerFace@users.noreply.github.com>
2023-03-05 14:59:03 -0500

    Updated right high goal auton

--- 7701V/src/autons/right-high-goal.cpp
+++ 7701V/src/autons/right-high-goal.cpp
@@ -2,7 +2,8 @@
 #include "control.h"
 
 void autons::rightHighGoal() {
-  flyMtrs.spin(fwd, 0.9125 * 12, volt);
+  flyMtrs.spin(fwd, 0.9 * 12, volt);
+  angler.set(true);
 
   // roller
   driveRelative(23, 300, 1700);
@@ -14,12 +15,10 @@ void autons::rightHighGoal() {
   driveRelative(-2.4166, 0, 1000);
 
   // shoot preloads
-  turnToAngle(-1.9375, 200, 700);
-  this_thread::sleep_for(750);
+  turnToAngle(-1.7, 200, 700);
+  this_thread::sleep_for(250);
   Indexer.shootDisc();
-  this_thread::sleep_for(250);
-  angler.set(true);
-  this_thread::sleep_for(250);
+  this_thread::sleep_for(1000);
   Indexer.shootDisc();
   this_thread::sleep_for(250);
   angler.set(false);
@@ -27,16 +26,16 @@ void autons::rightHighGoal() {
   // intake discs
   // 2.67, 2.46, -2.28
   intakeMtrs.spin(fwd, 12, volt);
-  flyMtrs.spin(fwd, 0.85 * 12, volt);
-  turnToAngle(2.815, 250, 1750);
+  flyMtrs.spin(fwd, 0.82 * 12, volt);
+  turnToAngle(2.8, 250, 1750);
   // driveRelative(80, 400, 2500, {0.0005, 0.000001, 0.000025});
   driveMtrs.spin(fwd, 6, volt);
   this_thread::sleep_for(2150);
   driveMtrs.stop();
 
   //shoot discs
-  turnToAngle(-1.9125, 300, 1500, 1, {0.8, 0, 0.0075});//-2.325
-  this_thread::sleep_for(100);
+  turnToAngle(-2.175, 300, 1500, 0.75, {0.6, 0, 0.0075});//-2.325
+  this_thread::sleep_for(500);
   // 111.25, 34 + 13/16
   // turnToAngle(M_PI + std::atan2((54+13/16) - pt::y(), 111.25 - pt::x()), 300, 1500);
   Indexer.shootDisc();
