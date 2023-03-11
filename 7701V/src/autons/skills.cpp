fae1dd536d04b1c8d73efbeaed308916a8af29fc
xTigerFace <34776435+xTigerFace@users.noreply.github.com>
2023-03-11 01:27:53 -0500

    Updated skills auton
    Starts with match loads, then roller on side wall, roller on starting wall, and endgame

--- 7701V/src/autons/skills.cpp
+++ 7701V/src/autons/skills.cpp
@@ -2,18 +2,30 @@
 #include "control.h"
 
 void autons::skills() {
-  flyMtrs.spin(fwd, 0.79 * 12, volt);
+  flyMtrs.spin(fwd, 0.85 * 12, volt);
   angler.set(true);
   this_thread::sleep_for(2500);
 
-  // starting match loads
+  // match loads 1
   matchLoadEnabled = true;
   this_thread::sleep_for(12500);
+  matchLoadEnabled = false;
 
-  // roller
-  turnToAngle(-0.4, 100, 1500, 0.5, {0.8, 0, 0.1});
-  driveRelative(36, 200, 1200);
-  turnToAngle(-M_PI_2, 75, 1000, 0.75);
-  driveRelative(12, 150, 600);
+  // roller 1
+  turnToAngle(-0.725, 100, 1500, 0.5, {0.8, 0, 0.1});
+  driveRelative(60, 200, 1500, 0.8);
+  turnToAngle(0.175, 75, 600, 0.4, {0.9, 0, 0.0075});
+  driveRelative(15, 150, 600);
   spinRoller(-1, rollerColor, 165);
+
+  // roller 2
+  driveRelative(-15, 200, 1500, 0.5);
+  turnToAngle(1.35, 75, 1500, 0.65, {0.9, 0, 0.0075});
+  driveRelative(24, 150, 1200, 0.65);
+  spinRoller(-1, rollerColor, 165);
+
+  // endgame
+  driveRelative(-12, 100, 1000);
+  turnToAngle(-0.7, 100, 1500, 0.5);
+  // expand();
 }
