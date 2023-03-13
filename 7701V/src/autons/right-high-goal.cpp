b45f269b39072ac9135e11daa4f1316f994417c6
xTigerFace <34776435+xTigerFace@users.noreply.github.com>
2023-03-13 08:44:20 -0400

    Auton changes for pphs

--- 7701V/src/autons/right-high-goal.cpp
+++ 7701V/src/autons/right-high-goal.cpp
@@ -2,11 +2,11 @@
 #include "control.h"
 
 void autons::rightHighGoal() {
-  flyMtrs.spin(fwd, 0.9375 * 12, volt);
+  flyMtrs.spin(fwd, 0.86 * 12, volt);
   angler.set(true);
 
   // roller
-  driveRelative(23, 300, 1700);
+  driveRelative(21, 300, 1700);
   turnToAngle(-M_PI_2 + 0.1, 600, 1200);
 
   driveRelative(12, 150, 600);
@@ -15,7 +15,7 @@ void autons::rightHighGoal() {
   driveRelative(-2.4166, 0, 1000);
 
   // shoot preloads
-  turnToAngle(-1.7, 200, 700);
+  turnToAngle(-1.8, 200, 700);
   this_thread::sleep_for(250);
   Indexer.shootDisc();
   this_thread::sleep_for(1000);
@@ -25,15 +25,15 @@ void autons::rightHighGoal() {
   // intake discs
   // 2.67, 2.46, -2.28
   intakeMtrs.spin(fwd, 12, volt);
-  flyMtrs.spin(fwd, 0.865 * 12, volt);
-  turnToAngle(2.85, 250, 1750);
+  flyMtrs.spin(fwd, 0.81 * 12, volt);
+  turnToAngle(2.85, 250, 1750, 0.8);
   // driveRelative(80, 400, 2500, {0.0005, 0.000001, 0.000025});
   driveMtrs.spin(fwd, 6, volt);
   this_thread::sleep_for(2150);
   driveMtrs.stop();
 
   //shoot discs
-  turnToAngle(-2.175, 300, 1500, 0.75, {0.6, 0, 0.0075});//-2.325
+  turnToAngle(-2.2, 300, 1500, 0.75, {0.6, 0, 0.0075});//-2.325
   this_thread::sleep_for(500);
   // 111.25, 34 + 13/16
   // turnToAngle(M_PI + std::atan2((54+13/16) - pt::y(), 111.25 - pt::x()), 300, 1500);
--- 7701V/src/autons/skills.cpp
+++ 7701V/src/autons/skills.cpp
@@ -2,30 +2,38 @@
 #include "control.h"
 
 void autons::skills() {
-  flyMtrs.spin(fwd, 0.85 * 12, volt);
+  flyMtrs.spin(fwd, flywheelMatchLoadCoeff * 12, volt);
   angler.set(true);
-  this_thread::sleep_for(2500);
+  this_thread::sleep_for(4000);
 
   // match loads 1
   matchLoadEnabled = true;
-  this_thread::sleep_for(12500);
+  this_thread::sleep_for(10000);
   matchLoadEnabled = false;
 
   // roller 1
-  turnToAngle(-0.725, 100, 1500, 0.5, {0.8, 0, 0.1});
+  turnToAngle(-0.8, 100, 1500, 0.5, {0.8, 0, 0.1});
+  intakeMtrs.spin(fwd, 12, volt);
   driveRelative(60, 200, 1500, 0.8);
   turnToAngle(0.175, 75, 600, 0.4, {0.9, 0, 0.0075});
-  driveRelative(15, 150, 600);
-  spinRoller(-1, rollerColor, 165);
+  driveRelative(36, 150, 3000);
+  spinRoller(-1, rollerColor, 190);
 
   // roller 2
-  driveRelative(-15, 200, 1500, 0.5);
+  intakeMtrs.spin(fwd, 12, volt);
+  driveRelative(-17, 200, 1500, 0.5);
   turnToAngle(1.35, 75, 1500, 0.65, {0.9, 0, 0.0075});
-  driveRelative(24, 150, 1200, 0.65);
-  spinRoller(-1, rollerColor, 165);
+  intakeMtrs.stop();
+  Indexer.shootDisc();
+  driveRelative(40, 150, 1200, 0.65);
+  Indexer.shootDisc();
+  this_thread::sleep_for(1000);
+  spinRoller(-1, rollerColor, 190);
 
   // endgame
   driveRelative(-12, 100, 1000);
-  turnToAngle(-0.7, 100, 1500, 0.5);
-  // expand();
+  turnToAngle(0.75, 100, 1500, 0.5);
+  if (Competition.isCompetitionSwitch() || Competition.isFieldControl()) {
+    expand();
+  }
 }
