17148567624525279cec204c5d6065989e52f3e9
xTigerFace <34776435+xTigerFace@users.noreply.github.com>
2023-04-15 12:01:15 -0400

    Uses both optical sensors for roller

--- 7701V/src/autons/test.cpp
+++ 7701V/src/autons/test.cpp
@@ -2,9 +2,5 @@
 #include "control.h"
 
 void autons::test() {
-  // spinRoller(-1, rollerColor, 165);
-
-  flyMtrs.spin(fwd, 0.825 * 12, volt);
-  matchLoadEnabled = true;
-  waitUntil(Controller.ButtonB.pressing());
+  spinRoller(-1, rollerColor, 165);
 }
--- 7701V/src/control.cpp
+++ 7701V/src/control.cpp
@@ -51,18 +51,31 @@ void spinRoller(double velocity, color col, int minTime, int maxTime) {
 
   waitUntil(rollerTimer.time(msec) > minTime || minTime == 0);
   while (true) {
-    int hue = leftRollerOptical.hue(); // TODO: use both optical sensors
+    double leftHue = leftRollerOptical.hue();
+    double rightHue = rightRollerOptical.hue();
 
     if (rollerTimer.time(msec) > maxTime && maxTime != 0) {
       break;
     }
 
-    if (col == red && (redMax > hue || hue > redMin)) {
-      break;
+    if (leftRollerOptical.isNearObject()) {
+      if (col == red && (redMax > leftHue || leftHue > redMin)) {
+        break;
+      }
+
+      if (col == blue && blueMax > leftHue && leftHue > blueMin) {
+        break;
+      }
     }
 
-    if (col == blue && blueMax > hue && hue > blueMin) {
-      break;
+    if (rightRollerOptical.isNearObject()) {
+      if (col == red && (redMax > rightHue || rightHue > redMin)) {
+        break;
+      }
+
+      if (col == blue && blueMax > rightHue && rightHue > blueMin) {
+        break;
+      }
     }
 
     this_thread::sleep_for(1);
