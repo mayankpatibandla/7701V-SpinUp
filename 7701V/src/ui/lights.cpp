574a7ef22fffde697820445029c164d39a656a2f
xTigerFace <34776435+xTigerFace@users.noreply.github.com>
2023-04-22 00:23:08 -0400

    Added alliance color to lights

--- 7701V/src/ui/lights.cpp
+++ 7701V/src/ui/lights.cpp
@@ -5,11 +5,20 @@ vex::thread lightsThread;
 void lightsInit() { lightsThread = vex::thread(lights); }
 
 void lights() {
-  sylib::Addrled addrleds = sylib::Addrled(22, 8, 64);
-  addrleds.gradient(0xFF00FF, 0x00FF00);
-  addrleds.cycle(*addrleds, 15);
+  sylib::Addrled frontLights(22, 8, 64);
+  frontLights.clear();
+
+  uint32_t allianceColor = selectedAuton.allianceColor == RED    ? 0xFF0000
+                           : selectedAuton.allianceColor == BLUE ? 0x0000FF
+                                                                 : 0x00FF00;
+
+  for (int i = 0; i < 64; i++) {
+    frontLights.set_pixel(allianceColor, i);
+    this_thread::sleep_for(2000 / 64);
+  }
 
   while (true) {
-    this_thread::sleep_for(0xFFFFFFFF);
+    frontLights.pulse(0xFFFFFF, 2, 10);
+    this_thread::sleep_for(2500);
   }
 }
