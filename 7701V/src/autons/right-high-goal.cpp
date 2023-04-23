c9db6f1545fe2465f6bcd78886e502a28af1e936
Mayank Patibandla <34776435+mayankpatibandla@users.noreply.github.com>
2023-04-23 02:37:21 -0400

    Completed right high goal auton

--- 7701V/src/autons/right-high-goal.cpp
+++ 7701V/src/autons/right-high-goal.cpp
@@ -5,16 +5,41 @@ void autons::rightHighGoal() {
   angler.set(true);
 
   // roller
-  driveRelative(21, 300, 1700);
+  driveRelative(22.5, 300, 1700);
   turnToAngle(-M_PI_2 + 0.1, 600, 1200);
 
   driveRelative(12, 150, 600);
   spinRoller(-1, rollerColor, 165);
 
-  driveRelative(-2.4166, 0, 1000);
+  driveRelative(-2.15, 25, 200);
 
   // get disc
-  turnToAngle(2.65, 300, 1500, 0.7);
+  turnToAngle(2.6, 300, 1500);
+  Flywheel.setTargetVelocity(0.67);
   intakeMtrs.spin(fwd, 12, volt);
-  driveRelative(35, 100, 2000, 0.85);
+  driveRelative(38, 100, 2500);
+
+  // shoot discs
+  turnToAngle(-2.06, 100, 1200, 0.6, {0.375, 0, 0});
+  repeat(3) {
+    Indexer.shootDisc();
+    Flywheel.setTargetVelocity(0.77);
+    this_thread::sleep_for(500);
+  }
+
+  // get discs
+  turnToAngle(2.34, 0, 1000);
+  Flywheel.setTargetVelocity(0.6475);
+  driveRelative(26, 0, 1000);
+  turnToAngle(-2.2, 0, 1000);
+  driveRelative(15, 0, 800);
+
+  // shoot discs
+  driveRelative(-12, 0, 1000);
+  turnToAngle(-1.975, 0, 1000, 0.75, {0.425, 0, 0});
+  repeat(3) {
+    Indexer.shootDisc();
+    Flywheel.setTargetVelocity(0.73);
+    this_thread::sleep_for(500);
+  }
 }
