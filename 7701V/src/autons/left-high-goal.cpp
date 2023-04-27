3cb19689d17749617f50ff77c7856d68baf20a4e
Mayank Patibandla <34776435+mayankpatibandla@users.noreply.github.com>
2023-04-26 22:47:05 -0400

    End of day 2

--- 7701V/src/autons/left-high-goal.cpp
+++ 7701V/src/autons/left-high-goal.cpp
@@ -2,28 +2,20 @@
 
 void autons::leftHighGoal() {
   Flywheel.setTargetVelocity(1);
-  intakeMtrs.spin(fwd, 12, volt);
   angler.set(true);
 
-  // get disc
-  turnToAngle(-0.775, 0, 1000);
-  driveRelative(11, 75, 900, 0.75);
-  this_thread::sleep_for(1000);
-  driveRelative(-3, 50, 500);
-
   // roller
-  turnToAngle(M_PI_2, 0, 1000);
   driveRelative(12, 150, 600);
-  spinRoller(-1, rollerColor, 165, 500);
+  spinRoller(-1, rollerColor, 165, 750);
   Flywheel.setTargetVelocity(0.7);
-  driveRelative(-4, 0, 600);
+  driveRelative(-3, 0, 350);
 
   // knock over 3 stack
-  turnToAngle(0.46, 0, 1000);
+  turnToAngle(-0.675, 0, 1000);
   driveRelative(-30, 0, 1200, 0.7);
 
   // shoot
-  turnToAngle(2.3, 100, 1200, 0.6, {0.375, 0, 0});
+  turnToAngle(2.2, 100, 1200, 0.6, {0.375, 0, 0});
   repeat(3) {
     Indexer.shootDisc();
     Flywheel.setTargetVelocity(0.77);
